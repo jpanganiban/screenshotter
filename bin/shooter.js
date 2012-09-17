75a2d6565ca682a9bf7c75a50af776a4cd3bfc65
Jesse Panganiban <me@jpanganiban.com>
2012-09-17 22:10:12 +0800

    Log

--- bin/shooter.js
+++ bin/shooter.js
@@ -2,6 +2,9 @@ var page = new WebPage();
 var url = phantom.args[0];
 var filename = phantom.args[1];
 
+console.log(url);
+console.log(filename);
+
 
 page.viewportSize = {width: 1024, height: 768};
 page.open(url, function(status) {
