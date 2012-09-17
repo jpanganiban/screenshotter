c5b9e07c00a213aebe673f5492951530ac716b00
Jesse Panganiban <me@jpanganiban.com>
2012-09-17 22:08:15 +0800

    Added shooter script

---
+++ bin/shooter.js
@@ -0,0 +1,12 @@
+var page = new WebPage();
+var url = phantom.args[0];
+var filename = phantom.args[1];
+
+
+page.viewportSize = {width: 1024, height: 768};
+page.open(url, function(status) {
+
+    page.render(filename);
+    phantom.exit();
+
+});
