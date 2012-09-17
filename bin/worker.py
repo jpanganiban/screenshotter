3996b43f495e5414e982eb8f32860465e866c253
Jesse Panganiban <me@jpanganiban.com>
2012-09-17 23:11:09 +0800

    Asdf

--- bin/worker.py
+++ bin/worker.py
@@ -9,8 +9,8 @@ worker = gearman.GearmanWorker(config.GEARMAN_HOSTS)
 
 def take_screenshot(gm_worker, gm_job):
     savepath = utils.save_path(gm_job.data['url'], gm_job.unique)
-    return utils.take_screenshot(gm_job.data['url'], savepath)
-    data = {'url': gm_job.data['url'], 'filename': savepath}
+    data = utils.take_screenshot(gm_job.data['url'], savepath)
+    print data
     return data
 
 if __name__ == '__main__':
